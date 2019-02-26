ce649eac391c240ebf66065c8057008453d228eb
XinLiu92 <xliu0410@gmail.com>
2019-02-25 22:58:23 -0500

    debug

--- QueryExpaison/src/main/java/QueryExpansion.java
+++ QueryExpaison/src/main/java/QueryExpansion.java
@@ -192,7 +192,7 @@ public class QueryExpansion {
 
     public CharArraySet getStopWordSet(){
         //String stopWordDir = "/home/xl1044/ds/Query_Expansion/QueryExpaison/File/stop_word.cfg";
-        String stopWordDir = QueryExpansion.class.getClassLoader().getResource("stop_word.cfg").getPath();
+        String stopWordDir = "src/main/resources/stop_word.cfg";
 
         List<String> list = new ArrayList<>();
 
